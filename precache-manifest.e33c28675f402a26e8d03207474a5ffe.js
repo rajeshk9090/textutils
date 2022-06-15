90ed6e35fc9f64529ae547a08e6affe3d76ec2f7
rajeshk9090 <rajeshk.brainerhub@gmail.com>
2022-06-15 12:04:43 +0530

    Updates

---
+++ precache-manifest.e33c28675f402a26e8d03207474a5ffe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "50caccf359fa0d723d95",
+    "url": "/textutils/static/css/main.313cd963.chunk.css"
+  },
+  {
+    "revision": "50caccf359fa0d723d95",
+    "url": "/textutils/static/js/main.50caccf3.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/textutils/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "b819952d326a6c22b6cea63eb7f359a1",
+    "url": "/textutils/index.html"
+  }
+];
